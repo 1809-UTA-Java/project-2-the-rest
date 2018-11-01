d32d0e0bf32263165bf84c707b1c3950d3bfc7da
shawnp95 <hwbyersshawn@gmail.com>
2018-11-01 12:03:14 -0500

    first

---
+++ MuleSoft/Spring/Project2_Cheese/src/main/java/com/revature/model/Cheese.java
@@ -0,0 +1,45 @@
+package com.revature.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "Cheese")
+public class Cheese {
+
+	@Id
+	int Id;
+	String Name;
+	int Quantity;
+
+	public int getId() {
+		return Id;
+	}
+
+	public void setId(int id) {
+		Id = id;
+	}
+
+	public String getName() {
+		return Name;
+	}
+
+	public void setName(String name) {
+		Name = name;
+	}
+
+	public int getQuantity() {
+		return Quantity;
+	}
+
+	public void setQuantity(int quantity) {
+		Quantity = quantity;
+	}
+
+	@Override
+	public String toString() {
+		return "Cheese [Id=" + Id + ", Name=" + Name + ", Quantity=" + Quantity + "]";
+	}
+
+}
