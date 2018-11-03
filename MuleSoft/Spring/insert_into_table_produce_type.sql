d957ecfa17d385efd65e685f67876830b91bbada
Leonardo DeLeon <lqdeleon@gmail.com>
2018-11-03 13:07:44 -0500

    add produce api sql scripts

---
+++ MuleSoft/Spring/insert_into_table_produce_type.sql
@@ -0,0 +1,25 @@
+INSERT INTO produce_type (id, produce_type)
+VALUES (20, 'lettuce');
+
+INSERT INTO produce_type (id, produce_type)
+VALUES (21, 'tomato');
+
+INSERT INTO produce_type (id, produce_type)
+VALUES (22, 'cucumber');
+
+INSERT INTO produce_type (id, produce_type)
+VALUES (23, 'spinach');
+
+INSERT INTO produce_type (id, produce_type)
+VALUES (24, 'avocado');
+
+INSERT INTO produce_type (id, produce_type)
+VALUES (25, 'pickle');
+
+INSERT INTO produce_type (id, produce_type)
+VALUES (26, 'banana');
+
+INSERT INTO produce_type (id, produce_type)
+VALUES (27, 'mushroom');
+
+commit;
