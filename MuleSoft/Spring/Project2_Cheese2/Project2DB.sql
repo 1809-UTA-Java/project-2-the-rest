c699423af6bf5c11e3ee93eace5b8b308e60bcac
shawnp95 <hwbyersshawn@gmail.com>
2018-11-03 09:29:47 -0500

    SQL Script

---
+++ MuleSoft/Spring/Project2_Cheese2/Project2DB.sql
@@ -0,0 +1,25 @@
+CREATE USER Project2_Cheese IDENTIFIED BY Shawn DEFAULT TABLESPACE users TEMPORARY TABLESPACE temp QUOTA 10M ON users;
+GRANT connect, resource, create session, create table, create view to Project2_Cheese;
+
+CREATE TABLE Cheese (
+    Id INT Primary Key NOT NULL,
+    name VARCHAR2 (50) NOT NULL,
+    quantity INT
+);
+
+INSERT INTO Cheese(Id, name, quantity) 
+    VALUES(11, 'Chedder', 50);
+    
+INSERT INTO Cheese(Id, name, quantity) 
+    VALUES(22, 'Swiss', 20);
+    
+INSERT INTO Cheese(Id, name, quantity) 
+    VALUES(33, 'Mozzarella', 10);
+    
+INSERT INTO Cheese(Id, name, quantity) 
+    VALUES(44, 'Blue', 5);
+    
+INSERT INTO Cheese(Id, name, quantity) 
+    VALUES(55, 'Gouda', 2);
+    
+commit;
