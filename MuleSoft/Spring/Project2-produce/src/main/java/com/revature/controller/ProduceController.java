395451de53b23fc01f74f4f65830d2728345ea5e
LeonardoDeLeon <lqdeleon@gmail.com>
2018-11-03 15:18:17 -0700

    add getByType and getByName and Readme for api

--- MuleSoft/Spring/Project2-produce/src/main/java/com/revature/controller/ProduceController.java
+++ MuleSoft/Spring/Project2-produce/src/main/java/com/revature/controller/ProduceController.java
@@ -1,10 +1,11 @@
 package com.revature.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.revature.model.Produce;
@@ -21,9 +22,13 @@ public class ProduceController {
 		return produces;
 	}
 	
-//	@GetMapping("/produce/{name}")
-//	public Produce getByName(@PathVariable("name") String name) {
-//		return dao.findByName(name);
-//	}
-	
+	@GetMapping("/produce/type/{name}")
+	public Produce getByProduceType(@PathVariable("name") String name) {
+		return dao.findByProduceType(name);
+	}
+
+	@GetMapping("/produce/id/{id}")
+	public Optional<Produce> getById(@PathVariable("id") Integer id) {
+		return dao.findById(id);
+	}
 }
--- MuleSoft/Spring/Project2-produce/src/main/java/com/revature/repository/ProduceDao.java
+++ MuleSoft/Spring/Project2-produce/src/main/java/com/revature/repository/ProduceDao.java
@@ -1,7 +1,5 @@
 package com.revature.repository;
 
-//import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 //import org.springframework.transaction.annotation.Transactional;
@@ -10,6 +8,7 @@ import com.revature.model.Produce;
 
 @Repository
 public interface ProduceDao extends JpaRepository<Produce, Integer> {
-	//Produce findAll(String name);
-	//Produce findByName(String name);
+	
+	Produce findByProduceType(String produceType);
+	
 }
